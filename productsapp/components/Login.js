1beb0f7e7092094d4bcaa57dc2250bd34859af4d
LizavetaVanahel <lvvanagel@gmail.com>
2018-12-04 10:46:27 +0300

    Removed cyclic dependency

--- productsapp/components/Login.js
+++ productsapp/components/Login.js
@@ -1,54 +1,54 @@
 import * as React from 'react';
 import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
-import ProductsList from '/ProductsList';
 import { Font } from 'expo';
 import styles from './Styles';
-
+import ProductsList from './ProductsList';
 export default class Login extends React.Component<{}> {
-   componentDidMount() {
-    Font.loadAsync({
-      'vincHand': require('../assets/fonts/VINCHAND.ttf'),
-    });
+  constructor(props) {
+    super(props);
+    this.state = { showProductsList: false };
   }
-  constructor(props){
-    super(props)
-    this.state = { showResults: true };
+
+  componentDidMount() {
+    Font.loadAsync({
+      vincHand: require('../assets/fonts/VINCHAND.ttf'),
+    });
   }
 
   showProductsList = () => {
-       this.setState({ showResults: false })
-   }
+    this.setState({ showProductsList: true });
+  };
 
   render() {
-      if (this.state.showResults == true) {
-       return (
-      <View style={styles.container}>
-        <Image style={styles.loginLogo} source={require('../assets/cart.png')} />
-         <Text style={styles.title}>
-           Friday's shop
-        </Text>
-         <TextInput placeholderTextColor={'#39C2D7'}
-          style={styles.loginFieldText}
-          placeholder="email"
-        />
-        <TextInput  placeholderTextColor={'#39C2D7'}
-          style={styles.loginFieldText}
-          placeholder="Text box"
-        />
-          <TouchableOpacity onPress={this.showProductsList} >
-         <Text style={styles.button}>
-           login
-        </Text>
-    </TouchableOpacity>
-      </View>
-      ); } else {
-        return ( 
-          <View style = {styles.view}>
-          <ProductsList/>
-          </View>
-        );
-        }
+    if (!this.state.showProductsList) {
+      return (
+        <View style={styles.container}>
+          <Image
+            style={styles.loginLogo}
+            source={require('../assets/cart.png')}
+          />
+          <Text style={styles.title}>Friday's shop</Text>
+          <TextInput
+            placeholderTextColor={'#39C2D7'}
+            style={styles.loginFieldText}
+            placeholder="email"
+          />
+          <TextInput
+            placeholderTextColor={'#39C2D7'}
+            style={styles.loginFieldText}
+            placeholder="Text box"
+          />
+          <TouchableOpacity onPress={this.showProductsList}>
+            <Text style={styles.button}>login</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <ProductsList />
+        </View>
+      );
     }
+  }
 }
-
-
--- productsapp/components/Product.js
+++ productsapp/components/Product.js
@@ -1,49 +1,26 @@
 import * as React from 'react';
 import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
-import ProductsList from '/ProductsList';
-import { Font } from 'expo';
+import PropTypes from 'prop-types';
 import styles from './Styles';
 
-export default class Product extends React.Component<{}> {
-
-componentDidMount() {
-    Font.loadAsync({
-      'vincHand': require('../assets/fonts/VINCHAND.ttf'),
-    });
-  }
-  constructor(props){
-    super(props)
-    this.state = { showResults: true };
-  }
-
-  showProductsList = () => {
-       this.setState({ showResults: false })
-   }
-
-  render() {
-    if (this.state.showResults == true) {
-    return (
-      <View style={styles.container}>
-        <Image style={styles.poductLogo} source={require('../assets/cart.png')} />
-         <Text style={styles.title}>
-           Product 1
-        </Text>
-         <TextInput 
-          style={styles.productParagraph}
-          multiline = {true}
-          placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedoveiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' />
-          <TouchableOpacity onPress={this.showProductsList} >
-       <Text style={styles.button}>
-           All Products
-        </Text>
-    </TouchableOpacity>
-      </View>
-    ); } else {
-        return ( 
-          <View>
-          <ProductsList/>
-          </View>
-        );
-      }
-  }
+export default function Product({ label, logo, callback }) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.poductLogo} source={logo} />
+      <Text style={styles.title}>{label}</Text>
+      <TextInput
+        style={styles.productParagraph}
+        multiline={true}
+        placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedoveiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      />
+      <TouchableOpacity onPress={callback}>
+        <Text style={styles.button}>All Products</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+Product.propTypes = {
+  label: PropTypes.string,
+  logo: PropTypes.any,
+  callback: PropTypes.func.isRequired,
+};
